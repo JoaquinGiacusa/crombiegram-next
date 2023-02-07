e9841125bf2b9b2bd5035ec3275a9281fe549628
Francisco Donadio <francisco.donadio@hotmail.com>
2023-02-07 19:40:37 -0300

    add get post by id

---
+++ hooks/useContactPost.ts
@@ -0,0 +1,44 @@
+import { fetcher } from "@/utils/fetcher";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+export type ContactDataProps = {
+  id: string;
+  contentText: string;
+  imageName: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  like: {
+    id: string;
+    postId: string;
+    user: { firstName: string; lastName: string; profileImage: string };
+    userId: string;
+  }[];
+  comment: {
+    id: string;
+    comment: string;
+    userId: string;
+    postId: string;
+    createdAt: Date;
+    user: {
+      firstName: string;
+      lastName: string;
+      profileImage?: string;
+      position?: string;
+    };
+  }[];
+}[];
+
+const useContactPost = () => {
+  const route = useRouter();
+  const id = route.query.id;
+
+  const { data, error, isLoading, mutate } = useSWR<ContactDataProps>(
+    `/post/contact/${id}`,
+    fetcher
+  );
+
+  return { data, error, isLoading };
+};
+export default useContactPost;
